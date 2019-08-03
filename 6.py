4f39fb99109045310f8368516544220c25b64c85
Adiev-Ruslan <33130059+Adiev-Ruslan@users.noreply.github.com>
2019-08-03 18:01:45 +0300

    Update 6.py

--- 6.py
+++ 6.py
@@ -2,39 +2,3 @@
 with open('test_file.txt', encoding='utf-8') as tf:
     for some_shit in tf:
         print(some_shit, end='')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
