78d1d69135b657b07e5614e72411ff086f9856ff
ruslan <ruslan.adieff@yandex.ru>
2019-08-03 17:38:52 +0300

    first commit

---
+++ 6.py
@@ -0,0 +1,40 @@
+#6
+with open('test_file.txt', encoding='utf-8') as tf:
+    for some_shit in tf:
+        print(some_shit, end='')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
