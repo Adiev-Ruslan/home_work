f87c157c57a5bd277b1710bc4b0493c0dbb7ae4c
Adiev-Ruslan <33130059+Adiev-Ruslan@users.noreply.github.com>
2019-08-03 18:01:17 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -11,35 +11,3 @@ subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
 for line in subproc_ping.stdout:
     line = line.decode('cp866').encode('utf-8')
     print(line.decode('utf-8'))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
